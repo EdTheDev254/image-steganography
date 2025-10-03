124bb5ddb17775f2b2337c2e46e76b645ba8a2e2
Edwin Wagha <edwin.wagh20@gmail.com>
2025-10-04 01:47:10 +0300

    Update

--- TestingData/txt-to-binary.py
+++ TestingData/txt-to-binary.py
@@ -1,5 +1,11 @@
 my_message = "Hello, World!"
 
+binary_message = ''
+
+# for i in my_message:
+#     binary_message += format(ord(i), '08b')
+
+#Below is a single line statement and does the same thing as above loop
 binary_message = ''.join(format(ord(char), '08b') for char in my_message)
 
 print(binary_message)
